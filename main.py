31988046e413d280d5ff96f4a5d0cf0f8e1cb366
GStudent <1035193656@qqq.com>
2021-12-12 00:47:23 +0800

    修改print语句

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print('hi，dzzzhPyCharm')
+    print('helloword')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
