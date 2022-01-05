0d38f1f633733930dfdbd07bb96d18ed90379355
GStudent <1035193656@qqq.com>
2022-01-05 17:55:25 +0800

    modify maindemo.py again

--- maindemo.py
+++ maindemo.py
@@ -1,3 +1,4 @@
 def print_hello(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hello, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    print(f'Helloword, {name}')
