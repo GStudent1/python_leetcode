2bbf51425c7ca7432a79926dd8f0452eb3751d99
GStudent <1035193656@qqq.com>
2022-01-05 17:17:24 +0800

    test demo

--- main.py
+++ main.py
@@ -12,8 +12,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print('1')
-    print('2')
-    print('3')
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
