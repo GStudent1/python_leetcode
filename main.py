2636fb9290e18fec3894788abc12a46803b8db38
gaoshuang.aggie <gaoshuang.aggie@bytedance.com>
2022-05-02 18:29:00 +0800

    no message

--- main.py
+++ main.py
@@ -1,22 +1,53 @@
-# This is a sample Python script.
+from typing import Optional
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+"""
+两数相加
+"""
+def addTwoNumbers(l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+    temp = 0
+    c = ListNode()
+    e = c
+    while l1 != None or l2 != None or temp > 0:
+        val1 = l1.val if l1 != None else 0
+        val2 = l2.val if l2 != None else 0
+        total = val2 + val1 + temp
+        temp = total // 10
+        val3 = total % 10
+        d = ListNode()
+        d.val = val3
+        e.next = d
+        e = e.next
+        l1 = l1.next if l1 != None else None
+        l2 = l2.next if l2 != None else None
+    return c.next
+"""
+合并两个有序链表
+"""
+def mergeTwoLists(list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+    reslute=ListNode()
+    Listreslute1=reslute
+    while list1 !=None and list2 !=None:
+        c = ListNode()
+        if list1.val<=list2.val:
+            c.val = list1.val
+            list1=list1.next
+        else:
+            c.val = list2.val
+            list2 = list2.next
+        Listreslute1.next = c
+        Listreslute1 = c
+    Listreslute1.next=list1 if list1 !=None else list2
+    return reslute.next
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print('1')
-    print('2')
-    print('3')
-    print('4')
-    print('5')
-    print('6')
+    a=ListNode(1,ListNode(2,ListNode(3,ListNode(4))))
+    b=ListNode(1,ListNode(2,ListNode(3,ListNode(4,ListNode(5,ListNode(6))))))
+    print(mergeTwoLists(a,b))
+
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- maindemo.py
+++ maindemo.py
@@ -1,8 +1,72 @@
+from typing import List
+
+
+
+
+"""
+有效的括号
+"""
+def isValid(s: str) -> bool:
+    if s.count("(") != s.count(")") or s.count("{") != s.count("}") or s.count("[") != s.count("]"):
+        return False
+
+
+"""
+两数之和
+"""
+def twoSum(nums: List[int], target: int) -> List[int]:
+    c=[]
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[j] + nums[i] == target:
+                c.append(i)
+                c.append(j)
+    return c
+
+def twoSum2(nums: List[int], target: int) -> List[int]:
+    hashtable = dict()
+    for i, num in enumerate(nums):
+        if target - num in hashtable:
+            return [hashtable[target - num], i]
+        hashtable[nums[i]] = i
+    return []
+
+"""
+最小差值 I
+"""
+def smallestRangeI(nums: List[int], k: int) -> int:
+    return max(0,max(nums)-min(nums)-2*k)
+
+
+if __name__ == '__main__':
+    str = [1,3,6]
+    print(smallestRangeI(str,3))
+
+"""
+最后一个单词的长度
+"""
+def lengthOfLastWord( s: str) -> int:
+    slist=s.split()
+    laststr=slist[len(slist)-1]
+    return len(laststr)
+
+"""
+最长公共前缀
+"""
+def longestCommonPrefix (strs):
+    #以第一个字符串作为参照
+    for i in range(len(strs[0])):
+        #以第一个字符串的第i个字符作为参照
+        temp1 = strs[0][i]
+        #循环其他字符串的第i个字符，判断是否与第一个字符串的第i个字符相等，不相等则直接返回temp
+        for j in range(1,len(strs)):
+            if len(strs[j])==i or strs[j][i] != temp1:
+                return strs[0][:i]
+    return strs[0]
+
 """
 最长的回文子串
 """
-
-
 def longestPalindrome(s: str) -> str:
     temp = ''
     for i in range(len(s)):
@@ -20,8 +84,6 @@ def longestPalindrome(s: str) -> str:
                 temp = a
         return temp
 
-
-
 def rever(s: str):
     temp = ''
     i = len(s) - 1
@@ -30,18 +92,9 @@ def rever(s: str):
         i = i - 1
     return temp
 
-
-if __name__ == '__main__':
-    s = "abba"
-    #a=s[-4:-3]
-    a = longestPalindrome(s)
-    print(a)
-
 """
-重复字符的最长子串的长度
+无重复字符的最长子串的长度
 """
-
-
 def lengthOfLongestSubstring1(s: str) -> int:
     list = {}
     length = 0
@@ -58,7 +111,6 @@ def lengthOfLongestSubstring1(s: str) -> int:
             length = right - left
     return length
 
-
 def lengthOfLongestSubstring2(s: str) -> int:
     length = 0
     left = 0
@@ -72,7 +124,6 @@ def lengthOfLongestSubstring2(s: str) -> int:
         if right - left > length:
             length = right - left
     return length
-
 
 def lengthOfLongestSubstring3(s: str):
     # 哈希集合，记录每个字符是否出现过
@@ -91,3 +142,6 @@ def lengthOfLongestSubstring3(s: str):
         # 第 i 到 rk 个字符是一个极长的无重复字符子串
         ans = max(ans, rk - i + 1)
     return ans
+
+
+
